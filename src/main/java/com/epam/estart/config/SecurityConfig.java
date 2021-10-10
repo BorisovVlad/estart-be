e9ab5c769572f5352372410032b3dcf47359bcd8
Vasilii Azarov <vasilii_azarov@epam.com>
2021-10-10 21:31:20 +0300

    fix cors configuration

--- src/main/java/com/epam/estart/config/SecurityConfig.java
+++ src/main/java/com/epam/estart/config/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.epam.estart.config;
 
 import com.epam.estart.security.JwtAuthenticationEntryPoint;
 import com.epam.estart.security.JwtFilter;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -35,5 +40,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedMethods(Arrays.asList(
+        HttpMethod.GET.name(),
+        HttpMethod.HEAD.name(),
+        HttpMethod.POST.name(),
+        HttpMethod.PUT.name(),
+        HttpMethod.DELETE.name()));
+    corsConfiguration.setMaxAge(1800L);
+    source.registerCorsConfiguration("/**", corsConfiguration); // you restrict your path here
+    return source;
   }
 }
