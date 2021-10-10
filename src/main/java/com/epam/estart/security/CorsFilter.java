75e462d150f675294be21715c501491c6cc4cc87
Vladislav Borisov <Vladislav_Borisov@epam.com>
2021-10-10 22:26:32 +0300

    checkstyle fix

--- src/main/java/com/epam/estart/security/CorsFilter.java
+++ src/main/java/com/epam/estart/security/CorsFilter.java
@@ -17,15 +17,17 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
       ServletException {
-
     HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, "
+        + "Authorization");
     chain.doFilter(req, res);
   }
 
--- src/test/java/com/epam/estart/integration/UserControllerTest.java
+++ src/test/java/com/epam/estart/integration/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
 @Disabled
 @WebAppConfiguration
 @AutoConfigureMockMvc
